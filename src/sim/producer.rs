6760e93f7bc918974712372514a28885750a70bd
Taylor King <43785015+Gink3@users.noreply.github.com>
2021-03-17 02:01:00 -0500

    Added holding attribute

--- src/sim/producer.rs
+++ src/sim/producer.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 
 // Producer Class
 // pid - producer id
+// tid - trader id of owner
 // cost - cost calculated by the factors of the production from the controller
 // prod - hashmap with product name, amount pairs
 // needs - daily needs to proudce goods if not met no production occurs
@@ -10,22 +11,26 @@ use std::collections::HashMap;
 #[derive(Debug)]
 pub struct Producer<'a> {
     pid: usize,
+    tid: usize,
     cost: usize,
     prod: HashMap<&'a str, i64>,
     needs: HashMap<&'a str, i64>,
+    holds: HashMap<&'a str, i64>,
 }
 
 impl<'a> Producer<'a> {
     // Controller needs to provide p and c
     // p - producer id
     // c - cost
-    // 
-    pub fn new(p: usize,c: usize) -> Producer<'a> {
+    // holds - current holdings
+    pub fn new(p: usize,t: usize,c: usize) -> Producer<'a> {
         Producer {
             pid: p,
+            tid: t,
             cost: c,
             prod: HashMap::new(),
             needs: HashMap::new(),
+            holds: HashMap::new(),
         }
     }
     pub fn get_pid(self) -> usize {
@@ -42,7 +47,7 @@ mod tests {
 
     #[test]
     fn has_pid() {
-        let p = Producer::new(1,1000);
+        let p = Producer::new(1,0,1000);
         assert_eq!(1,p.get_pid());
     }
 
