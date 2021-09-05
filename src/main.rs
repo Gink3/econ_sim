9d6030d1861d70d3ee74104aa34cf2ed5e71d309
tlk0128 <taylorking5@my.unt>
2021-09-05 11:38:13 -0500

    Restarted simulation for a ground up redesign

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,6 @@ use crate::sim::Sim;
 
 
 fn main() {
-    // Set config file
-    let mut s: Sim = Sim::new("./sim_config/sim.cfg".to_string());
-    // write simulation data before initalization
-    let mut data = serde_json::to_string_pretty(&s).unwrap();
-    fs::write("./sim_out/s_debug_pre.dat", data).expect("Unable to write file");
-    // Initalize simulation and write inital state to output file
-    s.init();
-    data = serde_json::to_string_pretty(&s).unwrap();
-    fs::write("./sim_out/s_debug0.dat", data).expect("Unable to write file");
-    // Run simulation and write output to file
-    s.run(5);
-    data = serde_json::to_string_pretty(&s).unwrap();
-    fs::write("./sim_out/s_debug5.dat", data).expect("Unable to write file");
-
-    s.print_market();
-
+    let mut s = Sim::new();
+    s.init(5);
 }
--- src/sim.rs
+++ src/sim.rs
@@ -1,221 +1,50 @@
-use rand::prelude::*;
-use std::fs::File;
-use std::io::{self, prelude::*, BufRead, BufReader};
-use std::path::Path;
+
+use rand::Rng;
+
 use std::collections::HashMap;
-use serde::{Serialize, Deserialize};
 
-mod producer;
-use crate::sim::producer::Producer;
 
-mod trader;
-use crate::sim::trader::Trader;
+// s_price stock price
+// traders hashmap info
+// traders[0] - money
+// trader[1] - amount of stock owned
+#[derive(Debug)]
+pub struct Sim {
+    s_price: i64,
+    s_quantity:i64,
+    traders: Vec<Vec<i32>>,
 
-mod market;
-use crate::sim::market::Market;
-
-// Sim Class
-// Controller of the simulation
-// land - generated from a sim config file
-// poprate - percent of land, determines population
-// tRate - percent of land, determines trader count
-// next_tid - tracks next usable trader id
-// num_traders - number of traders in a simulation
-// traders - array that stores trader data
-// prods - array that stores production data
-// mp - market prices for all goods (item name, price)
-// avg_age - end of simulation metric
-#[derive(Debug, Serialize)]
-pub struct Sim<'a> {
-    //Statistics of the simulation
-    cfg: String,
-    land: usize,
-    prate: usize,
-    trate: usize,
-    next_tid: usize,
-    next_pid: usize,
-    num_traders: usize,
-    traders: Vec<Trader<'a>>,
-    prods: Vec<Producer<'a>>,
-    mp: HashMap<&'a str, u64>,
-    avg_age: u64,
-    daily_order: Vec<usize>,
-    market: Market,
 }
 
-impl<'a> Sim<'a> {
-    pub fn new(cpath: String) -> Sim<'static> {
+
+impl Sim {
+    pub fn new() -> Sim {
         Sim {
-            cfg: cpath,
-            land: 0,
-            prate: 0,
-            trate: 0,
-            next_tid: 1,
-            next_pid: 0,
-            num_traders: 0,
+            s_price: 100,
+            s_quantity: 1000,
             traders: Vec::new(),
-            prods: Vec::new(),
-            mp: HashMap::new(),
-            avg_age: 0,
-            daily_order: Vec::new(),
-            market: Market::new(),
         }
     }
-    // calculates and prints out simulation statistics
-    pub fn end_sim(&mut self) {
-        self.cal_avg_age();
-        println!("Average Age: {}", self.avg_age);
-        
+    // Simulation initialization function
+    // nt - number of traders
+    pub fn init(&mut self,nt: i32) {
+        for i in 0..nt {
+            let n = vec![1000,0];
+            self.traders.push(n)
+        }
     }
-    pub fn init(&mut self) {
-        // Reference https://doc.rust-lang.org/rust-by-example/std_misc/file/open.html
-        let path = Path::new(&self.cfg);
-        
-        let f = File::open(&path);
+    // Main simulation function
+    // t - number of time steps
+    pub fn run(&mut self, t: i32) {
+        for d in 0..t {
+            for trader in &self.traders {
 
-        let f = match f {
-            Ok(file) => file,
-            Err(error) => {panic!("{:?}", error);},
-        };
-        
-
-        let reader = BufReader::new(f);
-
-        for (index, line) in reader.lines().enumerate() {
-            let mut line = line.unwrap(); // Ignore errors.
-            // Devug print statement
-            //println!("{}",index);
-            match index {
-                0 => {
-                    line = line.replace("land:", "");
-                    self.land = line.parse::<usize>().unwrap();
-                }
-                1 => {
-                    line = line.replace("poprate:","");
-                    self.prate = line.parse::<usize>().unwrap();
-                }
-                2 => {
-                    line = line.replace("traderrate:","");
-                    self.trate = line.parse::<usize>().unwrap();
-                }
-                _ => {}
             }
         }
-
-        for _x in 0..10 {
-            self.create_traders();
-        }
-        self.num_traders = self.traders.iter().count();
-        self.market.add_good_to_market("Wood".to_string(),75, 1000);
     }
-    // Creates the inital vector of traders
-    fn create_traders(&mut self) {
-        let x = 0;
-        let t: Trader = Trader::new(self.next_tid,x);
-        self.next_tid+=1;
-
-        self.traders.push(t);
-    }
-    fn cal_avg_age(&mut self) {
-        let mut sum: u64 = 0;
-        for t in &self.traders {
-            sum = sum + u64::from(t.age);
-        }
-        sum = sum / self.traders.len() as u64;
-        self.avg_age = sum;
-    }
-    // Returns the index in the vector of trader
-    // given t - the tid of a given trader
-    // Returns as an Option so you must unwrap
-    fn get_index_tid(self, t: usize) -> Option<usize> {
-        for (index, trader) in self.traders.into_iter().enumerate() {
-            if trader.get_tid() == t {
-                return Some(index)
-            }
-        }
-        None
-    }
-    // Main function of this class, runs the simulation
-    // d - number of days the simulation runs for
-    pub fn run(&mut self, d: usize) {
-        for _day in 0..d {
-            //dsiabled temporarily due to copy trait issues
-            //self.gen_d_order();
-            for _t in &self.traders {
-                //traders action code goes here
-                _t.act();
-            }
-
-            self.daily_order.clear();
-        }
-    }
-
-    // Fills vector with a psuedo random order of indices
-    // used for iterating over trader objects at random
-    fn gen_d_order(&mut self) {
-        let mut rng = thread_rng();
-        for _t in 0..self.num_traders {
-            let mut added = false;
-            //println!("{:?}",t);
-            while !added {
-                let r: usize = rng.gen_range(0..self.num_traders);
-                //println!("{:?}",r);
-                if self.daily_order.contains(&r) {
-                    added = false;
-                } else {
-                    added = true;
-                    self.daily_order.push(r);
-                }
-                //println!("{:?}",self.daily_order);
-            }
-
-        }
-    }
-
-    pub fn print_market(self) {
-        println!("{:?}",self.market);
-    }
-
-    pub fn get_land(self) -> usize {
-        self.land
-    }
-    pub fn get_prate(self) -> usize {
-        self.prate
-    }
-    pub fn get_trate(self) -> usize {
-        self.trate
-    }
-
-}
-
-#[cfg(test)]
-#[allow(unused_imports)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn reads_land() {
-        let mut s = Sim::new("./sim_config/sample.cfg".to_string());
-        s.init();
-        assert_eq!(10000,s.get_land());
-    }
-    #[test]
-    fn reads_prate() {
-        let mut s = Sim::new("./sim_config/sample.cfg".to_string());
-        s.init();
-        assert_eq!(35,s.get_prate());
-    }
-    #[test]
-    fn reads_trate() {
-        let mut s = Sim::new("./sim_config/sample.cfg".to_string());
-        s.init();
-        assert_eq!(5,s.get_trate());
-    }
-    // NOTE tid is initially 1 greater than index
-    #[test]
-    fn finds_trader_index() {
-        let mut s = Sim::new("./sim_config/sample.cfg".to_string());
-        s.init();
-        assert_eq!(s.get_index_tid(5).unwrap(), 4);
+    fn trader_action(&self) -> i32 {
+        let mut rng = rand::thread_rng();
+        let num = rng.gen_range(0..4);
+        num
     }
 }
